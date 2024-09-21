d67290953bb9f789f48c6ac42066d8386d7e0837
phuponyy <119709202+phuponyy@users.noreply.github.com>
2024-09-21 23:32:56 +0700

    three commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+require("dotenv").config();
+
 // const Product = require("./src/models/product.model");
 const route = require("./src/routers/client/index.route");
 
@@ -11,11 +13,11 @@ app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
 
 // NOTE: Port
-const port = 3000;
+const port = process.env.PORT;
 
 // NOTE: route
 route(app);
 
 app.listen(port, () => {
-  console.log("Connect Success");
+  console.log(`Connect Success ${port}`);
 });
