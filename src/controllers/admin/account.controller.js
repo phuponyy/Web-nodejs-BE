2c5543f386c44585cf135499241c84488dbc0578
phuponyy <119709202+phuponyy@users.noreply.github.com>
2024-10-06 17:34:21 +0700

    up edit account

--- src/controllers/admin/account.controller.js
+++ src/controllers/admin/account.controller.js
@@ -52,3 +52,38 @@ module.exports.createPost = async (req, res) => {
     res.redirect(`${systemConfig.prefixAdmin}/accounts`);
   }
 };
+
+//NOTE: [GET] /admin/account/edit
+module.exports.edit = async (req, res) => {
+  const find = {
+    _id: req.params.id,
+    deleted: false,
+  };
+
+  try {
+    const data = await Account.findOne(find);
+    const roles = await Role.find({
+      deleted: false,
+    });
+
+    res.render("admin/pages/account/edit", {
+      pageTitle: "Chỉnh sửa tài khoản",
+      data: data,
+      roles: roles,
+    });
+  } catch (error) {
+    res.redirect(`${systemConfig.prefixAdmin}/accounts`);
+  }
+};
+
+//NOTE: [PATCH] /admin/account/edit/:id
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id;
+  if (req.body.password) {
+    req.body.password = md5(req.body.password);
+  } else {
+    delete req.body.password;
+  }
+  await Account.updateOne({ _id: id }, req.body);
+  res.redirect("back");
+};
